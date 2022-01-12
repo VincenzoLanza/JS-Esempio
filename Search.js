0d48e5a6367940fc859d18acd7735e3ddf17d93a
VincenzoLanza <vincenzo.lanza@aidia.it>
2022-01-12 17:28:43 +0100

    modifica esercizio

--- Search.js
+++ Search.js
@@ -1,9 +1,9 @@
 const axios=require("axios")
 const main = async () => {
-  const query = "et"
+  const query = process.argv.slice(2);
   const lista = await axios.get("https://jsonplaceholder.typicode.com/todos")
   const todos = lista.data
-  const todoValidi = todos.map((t) => t.title).filter((t) => t.includes(query))
+  const todoValidi = todos.map((t) => t.title).filter((t) => (query.some((qElem)=>t.includes(qElem))))
   console.log(todoValidi);
   // var stringhe = []
   // todos.forEach(el => stringhe.push(el.title))
